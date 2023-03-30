214db5a2ad60fb5d5fc7e3539f741f55451d2a3e
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2023-03-30 13:15:43 +0200

    Added data

---
+++ src/data/currencies.ts
@@ -0,0 +1,35 @@
+export const ratesPage1 = [
+  {currency: 'EUR', label: 'Euro', value: 0.1},
+  {currency: 'AED', label: 'Utd. Arab Emir. Dirham', value: 0.2},
+  {currency: 'AUD', label: 'Australian Dollar', value: 0.3},
+  {currency: 'BRL', label: 'Brazilian Real', value: 0.4},
+  {currency: 'CHF', label: 'Swiss Franc', value: 0.5},
+  {currency: 'CNY', label: 'Chinese Yuan Renminbi', value: 0.6},
+  {currency: 'GBP', label: 'Great British Pound', value: 0.7},
+  {currency: 'HKD', label: 'Hong Kong Dollar', value: 0.8},
+  {currency: 'THB', label: 'Thai Baht', value: 0.9},
+];
+
+export const ratesPage2 = [
+  {currency: 'INR', label: 'Indian Rupee', value: 0.11},
+  {currency: 'JPY', label: 'Japanese Yen', value: 0.12},
+  {currency: 'MXN', label: 'Mexican Peso', value: 0.13},
+  {currency: 'MYR', label: 'Malaysian Ringgit', value: 0.14},
+  {currency: 'PHP', label: 'Philippine Peso', value: 0.15},
+  {currency: 'SAR', label: 'Saudi Riyal', value: 0.16},
+  {currency: 'SEK', label: 'Swedish Krona', value: 0.17},
+  {currency: 'SGD', label: 'Singapore Dollar', value: 0.18},
+  {currency: 'ZAR', label: 'South African Rand', value: 0.19},
+];
+
+export const newRatesPage1 = [
+  {currency: 'EUR', label: 'Euro', value: 1.1},
+  {currency: 'AED', label: 'Utd. Arab Emir. Dirham', value: 1.2},
+  {currency: 'AUD', label: 'Australian Dollar', value: 1.3},
+  {currency: 'BRL', label: 'Brazilian Real', value: 1.4},
+  {currency: 'CHF', label: 'Swiss Franc', value: 1.5},
+  {currency: 'CNY', label: 'Chinese Yuan Renminbi', value: 1.6},
+  {currency: 'GBP', label: 'Great British Pound', value: 1.7},
+  {currency: 'HKD', label: 'Hong Kong Dollar', value: 1.8},
+  {currency: 'THB', label: 'Thai Baht', value: 1.9},
+];
