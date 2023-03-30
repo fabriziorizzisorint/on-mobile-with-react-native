dc6bf89d1fd854f49c0cb2b180ccf20576d155f1
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2023-03-30 16:49:41 +0200

    Improved currencies screen

--- src/screens/Currencies.tsx
+++ src/screens/Currencies.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {FlatList, View, Text, StyleSheet} from 'react-native';
-import {ratesPage1, ratesPage2} from '../data/currencies';
+import {newRatesPage1, ratesPage1, ratesPage2} from '../data/currencies';
 
 export default function CurrenciesScreen() {
   const [data, setData] = useState(ratesPage1);
@@ -14,21 +14,31 @@ export default function CurrenciesScreen() {
     }, 2000);
   };
 
+  const onRefresh = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setData(newRatesPage1);
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
     <FlatList
       data={data}
+      onRefresh={onRefresh}
       refreshing={loading}
-      renderItem={data =>
-        Currency({label: data.item.label, value: data.item.value})
-      }
+      renderItem={({item, index}) => (
+        <Currency label={item.label} value={item.value} odd={index % 2 === 0} />
+      )}
       onEndReached={onEndReached}
     />
   );
 }
 
-function Currency({label, value}: any) {
+function Currency({label, value, odd}: any) {
   return (
-    <View style={styles.itemContainer}>
+    <View
+      style={[styles.itemContainer, {backgroundColor: odd ? '#ddd' : '#aaa'}]}>
       <Text>{label}</Text>
       <Text>{value}</Text>
     </View>
@@ -40,6 +50,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 30,
-    height: 150,
+    height: 100,
+    borderWidth: 1,
+    margin: 20,
+    borderColor: '#888',
+    borderRadius: 10,
+    alignItems: 'center',
   },
 });
